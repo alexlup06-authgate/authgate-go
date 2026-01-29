73bcd1362d23ef481ea1e2be5f96e62764616fcb
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-01-29 15:18:54 +0100

    add helper for logout form

--- authgate/csrf.go
+++ authgate/csrf.go
@@ -2,6 +2,27 @@ package authgate
 
 import "net/http"
 
+type LogoutFormData struct {
+	Action    string
+	Method    string
+	CSRFName  string
+	CSRFValue string
+}
+
+func LogoutFormDataFromRequest(r *http.Request) (LogoutFormData, bool) {
+	token, ok := CSRFToken(r)
+	if !ok {
+		return LogoutFormData{}, false
+	}
+
+	return LogoutFormData{
+		Action:    "/auth/logout",
+		Method:    http.MethodPost,
+		CSRFName:  CSRFFormField,
+		CSRFValue: token,
+	}, true
+}
+
 func CSRFToken(req *http.Request) (string, bool) {
 	c, err := req.Cookie(CSRFCookieName)
 	if err != nil || c.Value == "" {
